ddaf07589a8a80846de5b032351d1ef997afa7f9
Bhuvneshwar <anavanav2222@gmail.com>
2025-11-19 16:47:35 +0530

    download1

--- Download/download.js
+++ Download/download.js
@@ -4,7 +4,10 @@ var h = document.querySelector("#btmh1")
 var bar = document.querySelector("#innerbar")
 var num = 50 + Math.floor(Math.random()*50)
 var card = document.querySelector("#card")
-
+var main = document.querySelector("main")
+var r = Math.floor(Math.random()*100)
+var g = Math.floor(Math.random()*100)
+var b = Math.floor(Math.random()*100)
 
 
 btn.addEventListener('click',function(){
@@ -15,11 +18,15 @@ btn.addEventListener('click',function(){
         grow++
         h.innerHTML = grow+"%"
         bar.style.width = grow+"%"
+        bar.style.backgroundColor = `rgb(${r},${g},${b})`
 
     },num)
     setTimeout(()=>{
         clearInterval(time)
         btn.innerHTML = "Downloaded"
+        btn.style.backgroundColor = `rgb(${r},${g},${b})`
+        main.style.backgroundColor = `rgb(${r},${g},${b})`
+        
         var h1 = document.createElement("h1")
         card.appendChild(h1)
         h1.innerHTML = `Your Download completed in ${num/10} Sec`
