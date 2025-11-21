186e1b7fc7dba911f912af97977483fcfb4091fc
Bhuvneshwar <anavanav2222@gmail.com>
2025-11-21 18:59:37 +0530

    imageHover

---
+++ Imagemove/script.js
@@ -0,0 +1,14 @@
+var hover = document.querySelectorAll(".hover")
+hover.forEach(function(elem){
+  elem.addEventListener("mouseenter",function(){
+    elem.childNodes[3].style.opacity = 1
+    elem.childNodes[3].style.scale = 1
+  })
+  elem.addEventListener("mousemove",function(dets){
+    elem.childNodes[3].style.left = dets.x+"px"
+  })
+  elem.addEventListener("mouseleave",function(){
+      elem.childNodes[3].style.scale = 0
+    elem.childNodes[3].style.opacity = 0
+  })
+})
