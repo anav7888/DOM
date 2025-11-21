0bc09e8769458ce510c0e1482e08ebe398b8a808
Bhuvneshwar <anavanav2222@gmail.com>
2025-11-21 21:23:12 +0530

    imagemove2

--- Imagemove/script.js
+++ Imagemove/script.js
@@ -6,6 +6,7 @@ hover.forEach(function(elem){
   })
   elem.addEventListener("mousemove",function(dets){
     elem.childNodes[3].style.left = dets.x+"px"
+    elem.childNodes[3].style.top = dets.y+"px"
   })
   elem.addEventListener("mouseleave",function(){
       elem.childNodes[3].style.scale = 0
