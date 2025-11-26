665c886d3cf333e81d4e69462a0babf98d450735
Bhuvneshwar <anavanav2222@gmail.com>
2025-11-26 20:07:01 +0530

    reels

---
+++ reels/script.js
@@ -0,0 +1,223 @@
+let reels = [
+    {
+      username: "Anav_andoria",
+      ismuted:true,
+      profilePic: "https://plus.unsplash.com/premium_vector-1719858611039-66c134efa74d?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isFollowed: true,
+      description: "Kuch v nhi milla #lgaane ko #hullalalalal ",
+      isLiked: false,
+      likeCount: 124,
+      commentCount: 12,
+      shareCount: 4,
+      video: "./assests/raviii.mp4",
+    },
+    {
+      username: "Rahul_ctya",
+      ismuted:true,
+      profilePic: "https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isFollowed: true,
+      description: "Yeah ho ho ho #ha #hi #ho",
+      isLiked: false,
+      likeCount: 542,
+      commentCount: 33,
+      shareCount: 9,
+      video: "./assests/baali.mp4",
+    },
+    {
+      username: "Bhrt_gandu",
+      ismuted:true,
+      profilePic: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isFollowed: true,
+      description: "yeah gndu hau #gndu #bdignd",
+      isLiked: false,
+      likeCount: 310,
+      commentCount: 18,
+      shareCount: 7,
+      video: "./assests/amitabh.mp4",
+    },
+    {
+      username: "Love_mehra",
+      ismuted:true,
+      profilePic: "https://plus.unsplash.com/premium_photo-1691784781482-9af9bce05096?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isFollowed: true,
+      description:  "Trying this new pasta spot today and omg it’s perfection 🤤🍝",
+      isLiked: false,
+      likeCount: 980,
+      commentCount: 51,
+      shareCount: 16,
+      video: "./assests/food.mp4",
+    },
+    {
+      username: "Munish_gay",
+      ismuted:true,
+      profilePic: "https://images.unsplash.com/photo-1740252117013-4fb21771e7ca?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isFollowed: false,
+      description: "kaala bhaiya #ganduuuu #about #fairlovely",
+      isLiked: true,
+      likeCount: 1500,
+      commentCount: 89,
+      shareCount: 22,
+      video: "./assests/gandhi.mp4",
+    },
+    {
+      username: "CHandan",
+      ismuted:true,
+      profilePic: "https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isFollowed: false,
+      description: "Just letting the music take over today 💃🔥 Feeling the rhythm.",
+      isLiked: false,
+      likeCount: 221,
+      commentCount: 14,
+      shareCount: 5,
+      video: "./assests/fun.mp4",
+    },
+    {
+      username: "Raffik",
+      ismuted:true,
+      profilePic: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isFollowed: true,
+      description: "Dhokha Dhokha #raffik #mullabadmass",
+      isLiked: true,
+      likeCount: 760,
+      commentCount: 40,
+      shareCount: 13,
+      video: "./assests/baali.mp4",
+    },
+    {
+      username: "Hakuna",
+      ismuted:true,
+      profilePic: "https://plus.unsplash.com/premium_photo-1739178656495-8109a8bc4f53?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isFollowed: false,
+      description: "hulaa hulli #gulla #gulli #mulli",
+      isLiked: false,
+      likeCount: 450,
+      commentCount: 22,
+      shareCount: 8,
+      video: "./assests/show.mp4",
+    },
+    {
+      username: "Amitabh_bachan",
+      ismuted:true,
+      profilePic: "https://plus.unsplash.com/premium_photo-1739163838574-27c663e8a22b?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isFollowed: true,
+      description: "maa kaa bhdosa  agg 🔥 #ahhh #aaggg",
+      isLiked: false,
+      likeCount: 670,
+      commentCount: 27,
+      shareCount: 11,
+      video: "./assests/food.mp4",
+    },
+    {
+      username: "Kacha_kella",
+      ismuted:true,
+      profilePic: "https://images.unsplash.com/photo-1648138285309-6d2a9daf9fd7?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isFollowed: false,
+      description: "kacha kella #pakka #kella #kacha kella ",
+      isLiked: false,
+      likeCount: 890,
+      commentCount: 40,
+      shareCount: 15,
+      video: "./assests/fun.mp4",
+    },
+  ]
+  var allreels = document.querySelector(".allreels")
+
+
+ 
+  function instareels(){
+    let sum = ""
+    reels.forEach(function(elem,idx){
+      sum+= `<div class="reels">
+      <div class="img">
+      <video loop autoplay ${elem.ismuted ? 'muted':''} src=${elem.video} ></video>
+      <div class="muted" id="${idx}">
+      ${elem.ismuted?'<i class="ri-volume-mute-fill"></i>':'<i class="ri-volume-up-fill"></i>'}
+      </div>
+      </div>
+      
+      <div class="toppart">
+          <h1>Reels</h1>
+          <h2>Friends</h2>
+      </div>
+      <div class="sideicons">
+          <div class="mainlike" id="${idx}">
+        <h4> 
+         ${elem.isLiked? '<i class="ri-heart-3-fill" id="red"></i>':'<i class="ri-heart-3-line"></i>'} 
+        </h4>
+              <h1>${elem.likeCount}</h1>
+          </div>
+          <div class="maincomment" id="${idx}">
+          <h4> <i class="ri-chat-3-line"></i></h4>
+              <h1>${elem.commentCount}</h1>
+          </div>
+          <div class="mainshare">
+          <h4>  <i class="ri-send-plane-line"></i></h4>
+              <h1>${elem.shareCount}</h1>
+          </div>
+          <i class="ri-more-2-line"></i>
+          <img src="${elem.profilePic}" alt="">
+      </div>
+      <div class="profile">
+         <div class="name">
+          <img src="${elem.profilePic}" alt="">
+          <h1>${elem.username}</h1>
+          <button class="follow" id=${idx}>${elem.isFollowed?'Unfollow':'Follow'}</button>
+         </div> 
+         <h2>${elem.description}</h2>
+      </div>
+      
+  </div>`
+  
+  })
+  allreels.innerHTML = sum 
+  }
+  instareels()
+
+  allreels.addEventListener('click',function(dets){
+    
+    if(dets.target.className == 'mainlike'){
+      if(!reels[dets.target.id].isLiked){
+        reels[dets.target.id].likeCount++
+      reels[dets.target.id].isLiked = true
+      }else{
+        reels[dets.target.id].likeCount--
+      reels[dets.target.id].isLiked = false
+      }
+      instareels()
+    }
+    
+  
+    if(dets.target.className == 'maincomment'){
+      if(reels[dets.target.id].commentCount){
+        reels[dets.target.id].commentCount++
+      }else{
+        reels[dets.target.id].commentCount--
+      }
+      instareels()
+    }
+   
+    
+    if(dets.target.className == 'follow'){
+      if(!reels[dets.target.id].isFollowed){
+      reels[dets.target.id].isFollowed = true
+      }else{
+      reels[dets.target.id].isFollowed = false
+      }
+      instareels()
+    }
+    
+
+    if(dets.target.className == 'muted'){
+         if(!reels[dets.target.id].ismuted){
+            reels[dets.target.id].ismuted = true
+         }else{
+          reels[dets.target.id].ismuted = false
+         }
+         instareels()
+    }
+    
+
+  })
+
+ 
+
