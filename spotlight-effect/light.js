298f263fc3f7681b9c0e486f4dd7208a463a3984
Bhuvneshwar <anavanav2222@gmail.com>
2025-11-27 15:06:01 +0530

    spotlight1

--- spotlight-effect/light.js
+++ spotlight-effect/light.js
@@ -1,7 +1,10 @@
 var main = document.querySelector("main")
 
+window.addEventListener("click", () => {
+    document.querySelector("video").muted = false; 
+    document.querySelector("video").play();       
+});
 main.addEventListener('mousemove',(e)=>{
-    console.log(e.clientX,e.clientY)
-    main.style.setProperty("--x", e.clientX + "px")
-    main.style.setProperty("--y", e.clientY + "px")
+    document.body.style.setProperty("--y", e.clientY + "px")
+    document.body.style.setProperty("--x", e.clientX + "px")
 })
